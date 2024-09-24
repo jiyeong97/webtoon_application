24fd7ae766d8c7548ea09d98144d3c27cbdc9be1
jiyeong97 <106364020+jiyeong97@users.noreply.github.com>
2024-09-23 22:42:04 -0600

    Webtoon card

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -14,34 +14,79 @@ class HomeScreen extends StatelessWidget {
         elevation: 2,
         backgroundColor: Colors.green,
         foregroundColor: Colors.white,
-        
         title: const Center(
           child: Text(
             "Today's toons",
-            style: TextStyle(fontSize: 24, fontWeight: FontWeight.w600,),
+            style: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.w600,
+            ),
           ),
         ),
       ),
-
       body: FutureBuilder(
-        future: webtoons,
-         builder: (context, snapshot){
-          if(snapshot.hasData){
-            return ListView.separated(
-              scrollDirection: Axis.horizontal,
-              itemCount: snapshot.data!.length,
-              itemBuilder: (context, index) {
-                var webtoon = snapshot.data![index];
-                return Text(webtoon.title);
-              },
-              separatorBuilder: (context, index) =>
-              const SizedBox(width: 20,)
+          future: webtoons,
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              return Column(
+                children: [
+                  const SizedBox(
+                    height: 50,
+                  ),
+                  Expanded(child: makeList(snapshot))
+                ],
+              );
+            }
+            return const Center(
+              child: CircularProgressIndicator(),
             );
-          }
-          return const Center(
-            child: CircularProgressIndicator(),
-          );
-         }),
+          }),
     );
+  }
+
+  ListView makeList(AsyncSnapshot<List<WebtoonModel>> snapshot) {
+    return ListView.separated(
+        scrollDirection: Axis.horizontal,
+        itemCount: snapshot.data!.length,
+        padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+        itemBuilder: (context, index) {
+          var webtoon = snapshot.data![index];
+          return Column(
+            children: [
+              Container(
+                width: 250,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(25),
+                  boxShadow:[
+                    BoxShadow(
+                      blurRadius: 10,
+                      offset: const Offset(10, 10),
+                      color: Colors.black.withOpacity(0.5),
+                    )
+                  ]
+                ),
+                child: Image.network(
+                  webtoon.thumb,
+                  headers: const {
+                    'User-Agent':
+                        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36',
+                    'Referer': 'https://comic.naver.com',
+                  },
+                ),
+              ),
+              const SizedBox(height:10 ,),
+              Text(
+                webtoon.title,
+                style: const TextStyle(
+                  fontSize: 22,
+                  fontWeight: FontWeight.w400,
+                ),
+              )
+            ],
+          );
+        },
+        separatorBuilder: (context, index) => const SizedBox(
+              width: 40,
+            ));
   }
 }
